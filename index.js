c6a1f7d17fb09f56020cadd0325f4b726d85cc6b
atul-github <atul_pandit@yahoo.com>
2018-04-18 11:24:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * ©2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
+ * Bangalore, India. All Rights Reserved.
+ *
+ */
+
+module.exports = Object.assign(
+  {},
+  require('../lib/js-feel-cache-plugin'),
+  require('../lib/js-feel-external-function-plugin'),
+  require('../lib/js-feel-relations-plugin')
+);
